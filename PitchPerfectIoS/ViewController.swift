74838ee3b62d36c02a6038b0450262c3dd1b259b
Amartya Mondal <atm1504.in@gmail.com>
2021-12-07 18:29:34 +0530

    Add resources

--- PitchPerfectIoS/ViewController.swift
+++ PitchPerfectIoS/ViewController.swift
@@ -11,12 +11,25 @@ class ViewController: UIViewController {
     
     var count=0
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("View did load")
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("View will appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("View did appear")
+    }
 
     @IBAction func recordAudio(_ sender: Any) {
         count+=1
